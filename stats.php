faeee99cc302fe3cbd659e55141687d835e31c0e
andreaskomplex <andreas.lehmann@komplexitaeter.de>
2021-03-23 17:21:15 +0100

    - add event measurements to stats.php

--- stats.php
+++ stats.php
@@ -13,6 +13,12 @@
             <th>team registrations</th>
             <th>new round clicks</th>
             <th>average players</th>
+            <th>board on load</th>
+            <th>open info box</th>
+            <th>link simulation infos</th>
+            <th>link simulation login</th>
+            <th>open feedback box</th>
+            <th>open setup box</th>
         </tr>
 
 <?php
@@ -20,26 +26,44 @@ require './config.php';
 require './api/lib.php';
 
 $sql = "select d.day
-      ,ifnull(p.team_registrations_cnt, 0) as team_registrations_cnt
-      ,ifnull(r.new_round_cnt, 0) as new_round_cnt
-      ,ifnull(round(r.new_round_players_avg, 2), 0) as new_round_players_avg
-  from (select date_format(timestampadd(DAY,-1*i.n,current_timestamp), '%Y-%m-%d') as day
-        from (select 0 as n union select 1 union select 2 union  select 3
+     ,ifnull(p.team_registrations_cnt, 0) as team_registrations_cnt
+     ,ifnull(r.new_round_cnt, 0) as new_round_cnt
+     ,ifnull(round(r.new_round_players_avg, 2), 0) as new_round_players_avg
+    ,(select count(1) from pok_analytics_events_tbl
+        where date_format(creation_date, '%Y-%m-%d') = d.day
+        and event_code = 'BOARD_ON_LOAD') as board_on_load
+     ,(select count(1) from pok_analytics_events_tbl
+       where date_format(creation_date, '%Y-%m-%d') = d.day
+         and event_code = 'OPEN_INFO_BOX') as open_info_box
+     ,(select count(1) from pok_analytics_events_tbl
+       where date_format(creation_date, '%Y-%m-%d') = d.day
+         and event_code = 'LINK_SIMULATION_INFOS') as link_simulation_infos
+     ,(select count(1) from pok_analytics_events_tbl
+       where date_format(creation_date, '%Y-%m-%d') = d.day
+         and event_code = 'LINK_SIMULATION_LOGIN') as link_simulation_login
+     ,(select count(1) from pok_analytics_events_tbl
+       where date_format(creation_date, '%Y-%m-%d') = d.day
+         and event_code = 'OPEN_FEEDBACK_BOX') as open_feedback_box
+     ,(select count(1) from pok_analytics_events_tbl
+       where date_format(creation_date, '%Y-%m-%d') = d.day
+         and event_code = 'OPEN_SETUP_BOX') as open_setup_box
+from (select date_format(timestampadd(DAY,-1*i.n,current_timestamp), '%Y-%m-%d') as day
+      from (select 0 as n union select 1 union select 2 union  select 3
             union select 4 union select 5 union select 6 union select 7
             union select 8 union select 9 union select 10 union select 11
             union select 12 union select 13 union select 13 union select 14) as i) as d
-left outer join (select date_format(creation_date, '%Y-%m-%d') as day
-                      ,count(1) team_registrations_cnt
-                 from pok_teams_tbl
-                 group by day) as p
-on p.day = d.day
-left outer join (select date_format(creation_date, '%Y-%m-%d') as day
-                      ,count(1) new_round_cnt
-                      ,avg(players_count) new_round_players_avg
-                 from pok_roundstats_tbl
-                 where type_code = 'NEW_ROUND'
-                 group by day) as r
-on r.day = d.day
+         left outer join (select date_format(creation_date, '%Y-%m-%d') as day
+                               ,count(1) team_registrations_cnt
+                          from pok_teams_tbl
+                          group by day) as p
+                         on p.day = d.day
+         left outer join (select date_format(creation_date, '%Y-%m-%d') as day
+                               ,count(1) new_round_cnt
+                               ,avg(players_count) new_round_players_avg
+                          from pok_roundstats_tbl
+                          where type_code = 'NEW_ROUND'
+                          group by day) as r
+                         on r.day = d.day
 order by d.day desc
 ";
 
@@ -51,7 +75,13 @@ while(  $obj = $result->fetch_object()) {
     echo("\t\t\t<td>$obj->day</td>\n");
     echo("\t\t\t<td>$obj->team_registrations_cnt</td>\n");
     echo("\t\t\t<td>$obj->new_round_cnt</td>\n");
-    echo("\t\t\t<td>$obj->new_round_players_avg   </td>\n");
+    echo("\t\t\t<td>$obj->new_round_players_avg</td>\n");
+    echo("\t\t\t<td>$obj->board_on_load</td>\n");
+    echo("\t\t\t<td>$obj->open_info_box</td>\n");
+    echo("\t\t\t<td>$obj->link_simulation_infos</td>\n");
+    echo("\t\t\t<td>$obj->link_simulation_login</td>\n");
+    echo("\t\t\t<td>$obj->open_feedback_box</td>\n");
+    echo("\t\t\t<td>$obj->open_setup_box</td>\n");
     echo("\t\t</tr>\n\n");
 }
 
