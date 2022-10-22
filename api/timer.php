9e135de2776f2b9a9464e6504055b20716444cc5
andreaskomplex <andreas.lehmann@komplexitaeter.de>
2022-10-22 09:14:09 +0200

    - made the recently added timer restart function a bit more robust

--- api/timer.php
+++ api/timer.php
@@ -8,14 +8,25 @@ $action = filter_input(INPUT_GET, 'action', FILTER_SANITIZE_STRING);
 $link = db_init();
 
 if ($action=='start') {
+    $sql = $link->prepare( "SELECT if(isnull(timer_pause_time), 0
+         ,timestampdiff(SECOND, timer_pause_time, timer_start_time)) paused_time_value
+                                    FROM pok_teams_tbl as t
+                                    where t.id=?;");
+    $sql->bind_param('s', $t);
+    $sql->execute();
+    $result = $sql->get_result();
+    $obj = $result->fetch_object();
+    if ($obj == null)
+        $paused_time_value = 0;
+    else
+        $paused_time_value = $obj->paused_time_value;
+
+
     $sql = $link->prepare( "update pok_teams_tbl t
-                                 set t.timer_start_time = current_timestamp - (
-                                         ifnull(timer_pause_time, current_timestamp)
-                                       - ifnull(timer_start_time, current_timestamp)
-                                             )
+                                 set t.timer_start_time = timestampadd(SECOND, ?, current_timestamp)
                                     ,t.timer_pause_time = null
                                where t.id=?;");
-    $sql->bind_param('s', $t);
+    $sql->bind_param('is', $paused_time_value, $t);
     $sql->execute();
 }
 
