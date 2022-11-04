12d4b520aad04988c18fe0c15da47e3c27afbbba
andreaskomplex <andreas.lehmann@komplexitaeter.de>
2022-11-04 14:31:22 +0100

    - add new API endpoints for new topic feature

--- api/update_topic.php
+++ api/update_topic.php
@@ -1,11 +1,11 @@
 <?php
 require '../config.php';
 require './lib.php';
+error_log("call...");
 $t = substr( filter_input(INPUT_GET, "t", FILTER_SANITIZE_URL	) ,0,80);
 validate_team($t) or exit;
 
 $topic = substr( filter_input(INPUT_POST, "topic", FILTER_SANITIZE_STRING, FILTER_FLAG_NO_ENCODE_QUOTES	) ,0,2000);
-
 
 $link = db_init();
 
