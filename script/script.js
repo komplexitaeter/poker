d6ead1d62bdf993cc6f7c47fe1af1e0af85a63eb
andreaskomplex <andreas.lehmann@komplexitaeter.de>
2022-11-14 20:02:06 +0100

    - fixed some of the code inspection insturctions

--- script/script.js
+++ script/script.js
@@ -8,7 +8,7 @@ let gOnLoadFocus = null;
 
 
 String.prototype.hashCode = function(){
-    var hash = 0;
+    let hash = 0;
     if (this.length == 0) return hash;
     for (i = 0; i < this.length; i++) {
         char = this.charCodeAt(i);
