a4b4ffa3bf323adaf04f2851a6a9674af6cea139
francoiskomplex <francois.canonne@komplexitaeter.de>
2022-11-25 10:31:39 +0100

    - defined expected json structure for surveys

--- api/get_survey.php
+++ api/get_survey.php
@@ -22,4 +22,18 @@ while ($obj = $result->fetch_object()) {
 
 $link->close();
 
-echo json_encode(array("vote_results"=>$vote_results), JSON_UNESCAPED_UNICODE);
+$temp = '{
+  "survey_id": 1,
+  "votes_count": 200,
+  "survey_intro": "Currently working on a feature to sort the cards (after revealed) by values and not by player\'s names, like it is now). Would you like it...",
+  "vote_options": [
+    {"id": 1, "text": "no (leave as is)", "votes_count": 66, "votes_percentage": 33},
+    {"id": 2, "text": "to automatically sort", "votes_count": 42, "votes_percentage": 21},
+    {"id": 3, "text": "to have a button to sort", "votes_count": 32, "votes_percentage": 16},
+    {"id": 4, "text": "configurable over toggle", "votes_count": 60, "votes_percentage": 30}
+  ]
+}';
+
+//echo json_encode(array("vote_results"=>$vote_results), JSON_UNESCAPED_UNICODE);
+
+echo $temp;
