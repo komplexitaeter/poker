c535bbdf7b95af7020aa6a1ef7a27a9e688a2a42
andreaskomplex <andreas.lehmann@komplexitaeter.de>
2022-12-09 07:54:19 +0100

    - test.php

--- test.php
+++ test.php
@@ -5,7 +5,7 @@ require './api/lib.php';
 $id = substr( filter_input(INPUT_GET, "id", FILTER_SANITIZE_FULL_SPECIAL_CHARS	) ,0,16);
 $t = substr( filter_input(INPUT_GET, "t", FILTER_SANITIZE_URL	) ,0,80);
 for ($i=0;$i<10000;$i++) {
-    $dao = getDaoOLD($t, $id);
+    $dao = getDao($t, $id);
     $json = json_encode($dao, JSON_UNESCAPED_UNICODE);
     if ($i==0) echo $json;
 }
