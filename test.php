b9d9a2750fc27b63df57243913d640126ac145a4
andreaskomplex <andreas.lehmann@komplexitaeter.de>
2022-12-09 12:00:38 +0100

    - test.php

--- test.php
+++ test.php
@@ -9,7 +9,7 @@ $t = substr( filter_input(INPUT_GET, "t", FILTER_SANITIZE_URL	) ,0,80);
 for ($i=0;$i<10000;$i++) {
 
     $start2 = microtime(true);
-    $dao = getDao($t, $id);
+    $dao = getDaoI($t, $id, $end3);
     $end2 += (microtime(true) - $start2);
 
     $json = json_encode($dao, JSON_UNESCAPED_UNICODE);
@@ -20,3 +20,4 @@ for ($i=0;$i<10000;$i++) {
 $end = (microtime(true) - $start);
 echo "<br/><br/>elapsed time: ".number_format(round($end,3),3,',','') ;
 echo "<br/>cuml dao time: ".number_format(round($end2,3),3,',','') ;
+echo "<br/>cuml db-init time: ".number_format(round($end3,3),3,',','') ;
