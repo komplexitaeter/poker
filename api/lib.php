071bc4af216bd9beaedcff4759a94540423f6259
andreaskomplex <andreas.lehmann@komplexitaeter.de>
2023-01-09 20:05:39 +0100

    - moved get_survey into lib

--- api/lib.php
+++ api/lib.php
@@ -238,7 +238,6 @@ function getDao($t, $id) {
     $result = $sql->get_result();
     if ($obj = $result->fetch_object()) {
         if ($obj->user_exists == 0) {
-            require './survey_selector.php';
             $survey_id = get_survey_id($link, $t, $id);
             if ($survey_id != null) $survey = 'LOUD';
             $sql_i = $link->prepare("INSERT INTO pok_user_tbl(id,survey_id) values(?,?)");
@@ -251,7 +250,6 @@ function getDao($t, $id) {
                 elseif ($obj->survey_skipped != 0)  $survey = 'SILENT';
                 else $survey = 'LOUD';
             } elseif ($obj->survey_id == -1) {
-                require './survey_selector.php';
                 $survey_id = get_survey_id($link, $t, $id);
                 if ($survey_id != null) {
                     $survey = 'LOUD';
@@ -307,4 +305,28 @@ function getDao($t, $id) {
         "all_players_ready"=>$all_players_ready,
         "one_ore_more_player_ready"=>$one_ore_more_player_ready,
         "players"=>$players );
+}
+
+function get_survey_id($link, $team_id, $user_id) {
+    $survey_id = null;
+    $sql = $link->prepare(" select greatest(team_age, ifnull(related_teams_age, 0)) as approx_team_age
+                              from
+                            (
+                            select t.id, timestampdiff(DAY, t.creation_date, current_timestamp) team_age
+                            ,(SELECT max( timestampdiff(DAY, t2.creation_date, current_timestamp) )
+                              FROM pok_players_tbl p1, pok_players_tbl p2, pok_teams_tbl t2 where p1.team_id =t.id
+                                                                            and p2.id = p1.id
+                                                                            and p2.team_id != p1.team_id
+                                                                            and t2.id = p2.team_id) related_teams_age
+                              from pok_teams_tbl t
+                            where t.id = ?
+                            ) as x
+                            ;");
+    $sql->bind_param('s', $team_id);
+    $sql->execute();
+    $result = $sql->get_result();
+    if ($obj = $result->fetch_object()) {
+        //if ($obj->approx_team_age >= 30) $survey_id = 1;
+    }
+    return $survey_id;
 }
