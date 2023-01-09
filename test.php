8d673905bc945ee712d9fb18432c4c4f68bf5f3a
andreaskomplex <andreas.lehmann@komplexitaeter.de>
2023-01-09 20:09:48 +0100

    - moved get_survey into lib

--- test.php
+++ test.php
@@ -4,7 +4,7 @@ require './config.php';
 require './api/lib.php';
 $id = substr( filter_input(INPUT_GET, "id", FILTER_SANITIZE_FULL_SPECIAL_CHARS	) ,0,16);
 $t = substr( filter_input(INPUT_GET, "t", FILTER_SANITIZE_URL	) ,0,80);
-for ($i=0;$i<10000;$i++) {
+for ($i=0;$i<1000;$i++) {
     $dao = getDao($t, $id);
     $json = json_encode($dao, JSON_UNESCAPED_UNICODE);
     if ($i==0) echo $json;
