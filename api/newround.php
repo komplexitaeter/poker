80df82979d53af90cc8dfebf7f38ebbc7cf6c6cc
andreaskomplex <andreas.lehmann@komplexitaeter.de>
2024-01-28 08:21:20 +0100

    - added robots.txt and sitemap.xml - extend the historicization of round data

--- api/newround.php
+++ api/newround.php
@@ -8,7 +8,8 @@ $link = db_init();
 validate_team($t, $link) or exit;
 
 $sql = $link->prepare("insert into pok_roundstats_tbl(team_id, players_count, type_code, timer_start_time
-                            , timer_pause_time ,timer_visibility, topic)
+                            , timer_pause_time ,timer_visibility, topic, cardset_flags, results_order, show_avg
+                            , anonymous_mode)
                 select p.team_id
                      , count(1) players_count
                      , IF(count(1) = count(p.card_key), 'NEW_ROUND', 'CANCEL_ROUND') type_code
@@ -16,6 +17,10 @@ $sql = $link->prepare("insert into pok_roundstats_tbl(team_id, players_count, ty
                      , t.timer_pause_time
                      , t.timer_visibility
                      , t.topic
+                     , t.cardset_flags
+                     , t.results_order
+                     , t.show_avg
+                     , t.anonymous_mode
                   from pok_players_tbl p, pok_teams_tbl t
                  where p.team_id = ?
                    and t.id = p.team_id
@@ -23,6 +28,19 @@ $sql = $link->prepare("insert into pok_roundstats_tbl(team_id, players_count, ty
 
 $sql->bind_param('s', $t);
 $sql->execute();
+
+$sql = $link->prepare( "select last_insert_id() as last_id");
+$sql->execute();
+$result = $sql->get_result();
+$obj = $result->fetch_object();
+if ($obj != null && $obj->last_id != 0) {
+    $sql = $link->prepare( "insert into pok_roundstat_details_tbl
+                                    select ? roundstat_id, t.id as player_id, t.team_id, t.name, t.card_key
+                                    from pok_players_tbl t
+                                    where t.team_id = ?");
+    $sql->bind_param('is', $obj->last_id, $t);
+    $sql->execute();
+}
 
 $sql = $link->prepare( "update pok_teams_tbl t
                                      set t.anonymous_mode = t.anonymous_request_toggle
